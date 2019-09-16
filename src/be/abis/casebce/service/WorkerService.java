eda419d75e62cc50fe6b2cfecfcc68e0208a42a4
Sophie Roch <scallakim@hotmail.com>
2019-09-16 15:57:35 +0200

    Login API

--- src/be/abis/casebce/service/WorkerService.java
+++ src/be/abis/casebce/service/WorkerService.java
@@ -5,6 +5,7 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -15,6 +16,7 @@ import javax.ws.rs.core.Response.Status;
 
 import be.abis.casebce.converter.ApiConverter;
 import be.abis.casebce.exception.ApiError;
+import be.abis.casebce.model.api.Login;
 import be.abis.casebce.model.api.Worker;
 import be.abis.casebce.session.WorkerSessionRemote;
 
@@ -44,6 +46,25 @@ public class WorkerService {
 		} catch (Exception e) {
 			ApiError err = new ApiError("Impossible to get worker with id " + workerId,
 					Status.BAD_REQUEST.getStatusCode(), e.getMessage());
+			Response res = Response.status(Status.BAD_REQUEST).entity(err).build();
+			throw new WebApplicationException(err.getTitle(), res);
+		}
+		return worker;
+	}
+
+	@Path("login")
+	@POST
+	public Worker login(Login login) {
+		Worker worker = null;
+		try {
+			System.out.println("login with " + login.getLogin() + " and " + login.getPassword());
+			worker = new Worker();
+			worker.setLogin(login.getLogin());
+			worker.setFirstName(login.getLogin());
+			worker.setId(-1);
+			worker.setLastName(login.getLogin());
+		} catch (Exception e) {
+			ApiError err = new ApiError("Impossible to login", Status.BAD_REQUEST.getStatusCode(), e.getMessage());
 			Response res = Response.status(Status.BAD_REQUEST).entity(err).build();
 			throw new WebApplicationException(err.getTitle(), res);
 		}
