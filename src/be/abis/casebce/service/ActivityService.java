aeea6a9385a414427d12b4e37d99cc667ca34a05
Abdelilah <abdelilah.razzouk@student.ehb.be>
2019-09-16 11:47:15 +0200

    API method addActivity

--- src/be/abis/casebce/service/ActivityService.java
+++ src/be/abis/casebce/service/ActivityService.java
@@ -8,16 +8,22 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 import be.abis.casebce.converter.ApiConverter;
+import be.abis.casebce.exception.ActivityCanNotBeAdded;
 import be.abis.casebce.exception.ApiError;
 import be.abis.casebce.model.api.Activity;
 import be.abis.casebce.session.ActivitySessionRemote;
@@ -36,6 +42,20 @@ public class ActivityService {
 					"java:global/AbisCaseBCE_App/AbisCaseBCE_EJB/ActivitySession!be.abis.casebce.session.ActivitySessionRemote");
 		} catch (NamingException e) {
 			e.printStackTrace();
+		}
+	}
+
+	@Path("add")
+	@POST
+	public void addActivity(be.abis.casebce.model.Activity activity) {
+		if (activity != null)
+			session.createActivity(activity);
+		try {
+		} catch (Exception e) {
+			ApiError err = new ApiError("Impossible to add activity", Status.BAD_REQUEST.getStatusCode(),
+					e.getMessage());
+			Response res = Response.status(Status.BAD_REQUEST).entity(err).build();
+			throw new WebApplicationException(err.getTitle(), res);
 		}
 	}
 
